d8abb1e9ec42fd0584977b3d24d0f7c1e7f7753a
HristiyanK-IP <hristiyan.kyuchukov@infopulse.com>
2020-04-23 07:14:59 +0000

    First Commit - Hristiyan

---
+++ INFOPULSE_FIORI/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("INFOPULSE_FIORI.infopulse_fiori.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
